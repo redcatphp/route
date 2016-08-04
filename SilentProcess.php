510cb10a60d554a9de7bb467562cb732893379fd
jo <jo@surikat.pro>
2016-08-04 19:04:26 +0200

    add SilentProcess helper

---
+++ SilentProcess.php
@@ -0,0 +1,21 @@
+<?php
+namespace RedCat\Route;
+static class SilentProcess{
+	private static function register($callback, $debug=false){
+		if($debug){
+			register_shutdown_function($callback);
+			return;
+		}
+		
+		header("Content-Encoding: none");
+		header("Connection: close");
+		register_shutdown_function(function()use($callback){			
+			$size = ob_get_length();
+			header("Content-Length: {$size}");
+			ob_end_flush();
+			ob_flush();
+			flush();
+			call_user_func($callback);
+		});
+	}
+}
