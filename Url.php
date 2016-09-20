52cfbe7963a3258b649ef41e9f8ccd273d635f24
jo <jo@surikat.pro>
2016-09-20 18:36:51 +0200

    getUriWithParams

--- Url.php
+++ Url.php
@@ -120,8 +120,11 @@ class Url {
 	static function slug($string){
 		return strtolower(trim(preg_replace('~[^0-9a-z]+~i', '-', html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8')), '-'));
 	}
-	function addParamsToUri($uri, $params=[]){
+	function addParamsToUri($uri, $params){
 		$sep = strpos($uri,'?')===false?'?':'&';
 		return $uri.$sep.http_build_query($params);
+	}
+	function getUriWithParams($params=[]){
+		return $this->addParamsToUri($this->getUri(),$params);
 	}
 }
