758fc6e29e9d47c521c4858b51a0915023a4585e
jo <jo@surikat.pro>
2016-09-20 11:44:42 +0200

    remove iterate because no key support make it inconsistent

--- Request.php
+++ Request.php
@@ -114,11 +114,6 @@ class Request implements \ArrayAccess,\Iterator,\Countable,\JsonSerializable{
 			break;
 		}
 	}
-	function &iterate(){
-        foreach($this->data as &$v){
-            yield $v;
-        }
-    }
 	function post(){
 		return $this->server['REQUEST_METHOD']=='POST';
 	}
