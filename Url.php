5ca3a5f76cadeedd7dc08488944c5fd737823293
jo <jo@surikat.pro>
2016-09-20 18:35:48 +0200

    addParamsToUri

--- Url.php
+++ Url.php
@@ -120,4 +120,8 @@ class Url {
 	static function slug($string){
 		return strtolower(trim(preg_replace('~[^0-9a-z]+~i', '-', html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8')), '-'));
 	}
+	function addParamsToUri($uri, $params=[]){
+		$sep = strpos($uri,'?')===false?'?':'&';
+		return $uri.$sep.http_build_query($params);
+	}
 }
